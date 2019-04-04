db96e4f8ecf87f481f3e9bc6ec90a1f193a3ed13
chivyDISC <47510209+chivyDISC@users.noreply.github.com>
2019-04-04 11:05:01 +0700

    Up again

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from models import*
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
@@ -8,14 +8,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socket = SocketIO(app)
 db.init_app(app)
 
-@socket.on('message')
+@socketio.on('message')
 def handle_message(msg):
     message = Message(message = msg)
     db.session.add(message)
     db.session.commit()
     send(msg, broadcast=True)
 
+@socketio.on('my event', namespace='/test')
+def handle_my_custom_namespace_event(json):
+    print('received json: ' + str(json))
+
 @app.route("/")
 def index():
     messages = Message.query.all()
     return render_template("index.html", messages = messages)
+
+
