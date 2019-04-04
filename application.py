078f346b93f1ad29f96616d4b66292576fe7a33b
chivyDISC <47510209+chivyDISC@users.noreply.github.com>
2019-04-04 12:04:28 +0700

    Fix

--- application.py
+++ application.py
@@ -8,16 +8,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socket = SocketIO(app)
 db.init_app(app)
 
+@socket.on('gui')
+def nhanduoc(tin):
+    emit('phanhoi',tin, broadcast = True)
+
 @socket.on('message')
 def handle_message(msg):
     message = Message(message = msg)
     db.session.add(message)
     db.session.commit()
     send(msg, broadcast=True)
-
-@socket.on('my event', namespace='/test')
-def handle_my_custom_namespace_event(user):
-    send('my response', user, broadcast = True)
 
 @app.route("/")
 def index():
